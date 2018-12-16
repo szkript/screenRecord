e602f57a87de49f07fc232197fec1144d9ec1220
Szanyi krisztián <szkript@gmail.com>
2018-12-16 20:26:30 +0100

    Add files via upload

---
+++ screenRec.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import os
+import pyautogui
+
+"""
+Rec screen imgs into video without img save
+
+# alternative way (not tested)
+def save():
+    os.system("ffmpeg -r 1 -i img%01d.png -vcodec mpeg4 -y movie.mp4")
+"""
+# simple version for working with CWD
+output = "video/vids/test.avi"
+
+# get info from pic
+image = pyautogui.screenshot()
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+height, width, channels = image.shape
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+while(True):
+    try:
+        image = pyautogui.screenshot()
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        out.write(image)
+        StopIteration(0.5)
+    except KeyboardInterrupt:   # ctrl+c
+        print("stop recording")
+        break
+
+
+out.release()
+cv2.destroyAllWindows()
